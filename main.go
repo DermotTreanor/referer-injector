d1d94ae39159e107fb6a9b8c2cf7de8e20b838fd
Dermot Treanor <dermot.treanor1@gmail.com>
2024-05-30 11:10:53 +0100

    Created proxy request by parsing a hard coded url, cloning context, and altering RequestURI and Host of req

--- main.go
+++ main.go
@@ -1,15 +1,64 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
+	"log"
+	"reflect"
+	"net/url"
+	"context"
 )
 
-func main(){
+var addr string = "localhost:8080"
+func main() {
+	// response, err := http.Get("https://www.youtube.com")
+	// if err != nil{
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(response)
+
 	http.HandleFunc("/", our_handler)
-	http.ListenAndServe("localhost:8080", nil)
+	//Remember that this will run the handler as a goroutine. 
+	//So, expect concurrency if using a browser as another automatic favicon request launches
+	err := http.ListenAndServe(addr, nil)
+	if err != nil{
+		log.Println(err)
+	}
 }
 
-func our_handler(rw http.ResponseWriter, req *http.Request){
-	fmt.Fprintf(rw, "The method you used was: %s", req.Method)
+func our_handler(rw http.ResponseWriter, req *http.Request) {	
+	go proxy_request(req)
+	rw.Header()["Date"] = nil
+	for k, v := range req.Header{
+		rw.Header()[k] = v
+	}
+	fmt.Fprintf(rw, "The Headers we received are as follows:\n%s\n\n\nHere are the response headers to send to you:\n%s", req.Header, rw.Header())
+
+}
+
+func proxy_request(req *http.Request){
+	var err error
+	req.RequestURI = ""
+	req.URL, err = url.Parse("https://www.youtube.com/hello")
+	if err != nil{
+		fmt.Printf("PARSE ERROR, %v. TYPE %T\n\n.", err, err)
+	}
+	req = req.Clone(context.Background())
+	req.Host = "youtube.com"
+
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil{
+		fmt.Printf("GOT ERROR: %v. TYPE, %T\n\n", err, err)
+	}
+	fmt.Printf("%s\n\n\n", resp)
+}
+
+
+func reflecting(req *http.Request){
+	reflection := reflect.ValueOf(*req.URL)
+	for i := 0; i < reflection.Type().NumField(); i++{
+		field := reflection.Type().Field(i)
+		fmt.Printf("Type: %s. Key: %s. Value: %v\n\n", field.Name, field.Type, reflection.Field(i).Interface())
+	}
 }
